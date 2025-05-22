fec9aa277066a1e77b8a9f8a024c1e93793511a0
Gaurav Sehgal <gaurav.sehgal@vastdata.com>
2025-05-22 16:36:12 +0530

    Some fixes

--- src/function/gds/vector_search.cpp
+++ src/function/gds/vector_search.cpp
@@ -1391,7 +1391,7 @@ namespace kuzu {
                         reverseResults(results, reversed, dc);
                     }
                 }
-                searchLocalState->materialize(reversed, *sharedState->fTable, efSearch);
+                searchLocalState->materialize(reversed, *sharedState->fTable, k);
                 stats.vectorSearchTimeMetric->stop();
                 stats.totalPins->increase(bm->stats.totalPins);
                 stats.pinsDuration->increase(bm->stats.pinDurationNano);
